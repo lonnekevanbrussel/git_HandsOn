c44ab844828681dd463c73e00b1f143a43005402
Lonneke <me@example.com>
2023-02-15 14:02:42 +0100

    some comments added

--- seqClass.py
+++ seqClass.py
@@ -13,21 +13,21 @@ if len(sys.argv) == 1:
 
 args = parser.parse_args()
 
-args.seq = args.seq = args.seq.upper()
-if re.search('^[ACGTU]+$', args.seq):
-    if re.search('T', args.seq):
+args.seq = args.seq = args.seq.upper()			# transform sequence into upperclass
+if re.search('^[ACGTU]+$', args.seq):			# search for ACGTU in sequence
+    if re.search('T', args.seq):			# if T is in sequence
         print ('The sequence is DNA')
-    elif re.search('U', args.seq):
+    elif re.search('U', args.seq):			# if U is in sequence
         print ('The sequence is RNA')
     else:
         print ('The sequence can be DNA or RNA')
 else:
     print ('The sequence is not DNA nor RNA')
 
-if args.motif:
-    args.motif = args.motif.upper()
+if args.motif:						# if a motif is entered
+    args.motif = args.motif.upper()			# transform motif into upperclass
     print(f'Motif search enabled: looking for motif "{args.motif}" in sequence "{args.seq} "... ', end = '')
-    if re.search(args.motif, args.seq):
+    if re.search(args.motif, args.seq):			# if motif is found
         print("MOTIF FOUND")
     else:
         print("MOTIF NOT FOUND")
